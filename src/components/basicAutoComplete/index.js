8430a42313e7dc5c4630c9ec56099aace9be0093
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2017-10-01 18:06:51 +0100

    Component changes

--- src/components/basicAutoComplete/index.js
+++ src/components/basicAutoComplete/index.js
@@ -1,46 +1,45 @@
-import React from 'react'
-import Downshift from 'downshift'
+import React from 'react';
+import Downshift from 'downshift';
 
-function BasicAutocomplete({items, onChange}) {
- return (
-   <Downshift onChange={onChange}>
-     {({
-       getInputProps,
-       getItemProps,
-       isOpen,
-       inputValue,
-       selectedItem,
-       highlightedIndex
-     }) => (
-       <div>
-         <input {...getInputProps({placeholder: 'Favorite color ?'})} />
-         {isOpen ? (
-           <div style={{border: '1px solid #ccc'}}>
-             {items
-               .filter(
-                 i =>
-                   !inputValue ||
+function BasicAutocomplete({ items, onChange, placeholder }) {
+  return (
+    <Downshift onChange={onChange}>
+      {({
+        getInputProps,
+        getItemProps,
+        isOpen,
+        inputValue,
+        selectedItem,
+        highlightedIndex,
+      }) => (
+        <div>
+          <input {...getInputProps({ placeholder })} />
+          {isOpen ? (
+            <div style={{ border: '1px solid #ccc' }}>
+              {items
+                .filter(
+                  i =>
+                    !inputValue ||
                    i.toLowerCase().includes(inputValue.toLowerCase()),
-               )
-               .map((item, index) => (
-                 <div
-                   {...getItemProps({item})}
-                   key={item}
-                   style={{
-                     backgroundColor:
+                )
+                .map((item, index) => (
+                  <div
+                    {...getItemProps({ item })}
+                    key={item}
+                    style={{
+                      backgroundColor:
                        highlightedIndex === index ? 'gray' : 'white',
-                     fontWeight: selectedItem === item ? 'bold' : 'normal',
-                   }}
-                 >
-                   {item}
-                 </div>
-               ))}
-           </div>
-         ) : null}
-       </div>
-     )}
-   </Downshift>
- )
+                      fontWeight: selectedItem === item ? 'bold' : 'normal',
+                    }}>
+                    {item}
+                  </div>
+                ))}
+            </div>
+          ) : null}
+        </div>
+      )}
+    </Downshift>
+  );
 }
 
-export default BasicAutocomplete
+export default BasicAutocomplete;
---
+++ src/components/homePage/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import BasicAutoComplete from '../basicAutoComplete'
+
+class HomePage extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Dublin Coach</h1>
+                <BasicAutoComplete
+                placeholder="From"
+                items={['apple', 'orange', 'carrot']}
+                onChange={selectedItem => console.log(selectedItem)}/>
+                <BasicAutoComplete
+                placeholder="To"
+                items={['apple', 'orange', 'carrot']}
+                onChange={selectedItem => console.log(selectedItem)}/>
+                <img src="./logo.png" />
+            </div>
+        )
+    }
+}
+
+export default HomePage
