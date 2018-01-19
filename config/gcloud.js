e291fada91a5ebc8a77b2972df2f61c16b748e6c
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2018-01-19 14:13:06 +0000

    New gcloud details

--- config/gcloud.js
+++ config/gcloud.js
@@ -1,20 +1,21 @@
-const fs = require('fs');
-const slashes = require('slashes');
+const fs = require("fs");
+const slashes = require("slashes");
 
 let gcloud = {
-  type: 'service_account',
-  project_id: 'dublin-coach',
+  type: "service_account",
+  project_id: "dublin-coach-123",
   private_key_id: process.env.private_key_id,
   private_key: process.env.private_key,
-  client_email: 'circle-ci@dublin-coach.iam.gserviceaccount.com',
+  client_email: "circleci@dublin-coach-123.iam.gserviceaccount.com",
   client_id: process.env.client_id,
-  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-  token_uri: 'https://accounts.google.com/o/oauth2/token',
-  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  client_x509_cert_url: 'https://www.googleapis.com/robot/v1/metadata/x509/circle-ci%40dublin-coach.iam.gserviceaccount.com',
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://accounts.google.com/o/oauth2/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/circleci%40dublin-coach-123.iam.gserviceaccount.com"
 };
 
-gcloud.private_key = slashes.strip(gcloud.private_key)
+gcloud.private_key = slashes.strip(gcloud.private_key);
 
 const data = JSON.stringify(gcloud, null, 2);
-fs.writeFileSync('config/gcloud.json', data);
+fs.writeFileSync("config/gcloud.json", data);
