b32f293b06952b91d6ca2de9ced4793e687ad95a
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2018-02-04 13:53:44 +0000

    Removed playground

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import registerServiceWorker from './registerServiceWorker';
 import Helmet from 'react-helmet';
 
 import Homepage from './components/Homepage'
-import Playground from './components/Playground'
 
 const App = () => (
   <Router>
@@ -19,7 +18,6 @@ const App = () => (
         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
       </Helmet>
       <Route path="/" exact component={Homepage}/>
-      <Route path="/playground" component={Playground} />
     </div>
   </Router>
 )
