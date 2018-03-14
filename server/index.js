800f431dcbd589ea47a59dadbf797c9a63f8aa43
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2018-03-14 16:15:16 +0000

    Redirect all to www.

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ const PORT = 4000;
 
 const app = new Express();
 
+app.use(require('express-naked-redirect')())
+
 app.use(function(req, res, next) {
   if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
     if(req.get('Host') === 'localhost:4000' || typeof req.get('X-Forwarded-Proto')  === "undefined"){
