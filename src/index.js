b14757584b33fea859543257d78cc378e677ef9d
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2018-03-14 15:46:19 +0000

    Added Apollo cache persist

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloLink } from 'apollo-link';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { persistCache } from 'apollo-cache-persist';
 import { HttpLink } from 'apollo-link-http';
 
 import Loadable from 'react-loadable'
@@ -17,11 +18,19 @@ const API_HOST =
     ? 'https://api.dublincoachconcept.com/graphql'
     : 'http://localhost:3000/graphql';
 
+
+const cache = new InMemoryCache().restore(window.__APOLLO_STATE__);
+
+persistCache({
+  cache,
+  storage: window.localStorage,
+});
+
 const client = new ApolloClient({
   ssrForceFetchDelay: 100,
   connectToDevTools: true,
   link: new HttpLink({ uri: API_HOST }),
-  cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
+  cache: cache,
 });
 
 window.onload = () => {
