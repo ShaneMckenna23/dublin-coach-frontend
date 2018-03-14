f848c820c7cd56a7ce3512b9e4809865473ca606
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2018-03-14 19:05:05 +0000

    Removed console.logs

--- src/custom-sw.js
+++ src/custom-sw.js
@@ -1,7 +1,5 @@
 self.addEventListener('fetch', function(event) {
-  console.log("Fetch Deteched")
   if (event.request.url.indexOf('cloud.squidex.io') > -1) {
-    console.log("Fetch: cloud.squidex.io")
     event.respondWith(
       caches.open(cacheName).then(function(cache) {
         return cache.match(event.request).then(function (response) {
