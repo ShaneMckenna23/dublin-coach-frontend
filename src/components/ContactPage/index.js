6c283d098a8f5fddb5341ae8b1b3cca935053246
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2018-03-21 18:49:38 +0000

    Removed test component

--- src/components/ContactPage/index.js
+++ src/components/ContactPage/index.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import {Grid} from 'semantic-ui-react'
 
 class ContactPage extends Component {
+
+  componentDidMount () {
+    window.scrollTo(0, 0)
+  }
+
   render () {
     return (
       <Grid container stackable verticalAlign='middle'>
