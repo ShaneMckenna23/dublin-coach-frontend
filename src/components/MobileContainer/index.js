5f0271e85727acf36877eda44e1f9194d4844bf1
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2018-03-21 18:50:21 +0000

    Footer added to Mobile

--- src/components/MobileContainer/index.js
+++ src/components/MobileContainer/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavbarMobile from '../NavbarMobile'
 import {Container} from 'semantic-ui-react'
+import Footer from '../Footer'
 
 const MobileContainer = ({ children }) => (
   <div>
     <NavbarMobile />
     <section style={{ padding: '1.6em 0em' }} />
       {children}
+      <Footer />
   </div>
 )
 
