f77211f37885d195b6bfbcdcc35a815f81c65f4f
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2018-03-30 19:57:02 +0100

    Gcloud migration

--- config/gcloud.js
+++ config/gcloud.js
@@ -3,16 +3,16 @@ const slashes = require("slashes");
 
 let gcloud = {
   type: "service_account",
-  project_id: "dublin-coach-123",
+  project_id: "dublin-coach-199613",
   private_key_id: process.env.private_key_id,
   private_key: process.env.private_key,
-  client_email: "circleci@dublin-coach-123.iam.gserviceaccount.com",
+  client_email: "circle-ci@dublin-coach-199613.iam.gserviceaccount.com",
   client_id: process.env.client_id,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://accounts.google.com/o/oauth2/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   client_x509_cert_url:
-    "https://www.googleapis.com/robot/v1/metadata/x509/circleci%40dublin-coach-123.iam.gserviceaccount.com"
+    "https://www.googleapis.com/robot/v1/metadata/x509/circle-ci%40dublin-coach-199613.iam.gserviceaccount.com"
 };
 
 gcloud.private_key = slashes.strip(gcloud.private_key);
